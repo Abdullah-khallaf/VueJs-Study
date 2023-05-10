dfd1bb9c7adc5ac5267005ddb48e99687d9d5e50
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2023-05-10 19:03:58 +0300

    add components

---
+++ Vue-Project/src/components/App.vue
@@ -0,0 +1,42 @@
+<template>
+    <navBar></navBar>
+    <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import navBarVue from './navBar.vue';
+import pageViewer from './components/pageViewer.vue';
+
+
+export default {
+
+  components:{
+    navBarVue,
+    pageViewer,
+  },
+    data() {
+        //provide data to options
+        return {
+          activePage: 0,
+          useDarkNav: false,
+          pages: [
+            {
+              link: { text: "Home", url: "index.html" },
+              pageTitle: "Home",
+              content: "This Home Page",
+            },
+            {
+              link: { text: "About", url: "about.html" },
+              pageTitle: "About",
+              content: "This About Page",
+            },
+            {
+              link: { text: "Contact", url: "contact.html" },
+              pageTitle: "Contact",
+              content: "This Contact Page",
+            }
+          ]
+        };
+    },
+}
+</script>
---
+++ Vue-Project/src/components/navBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <nav :class="navBarClasses">
+      <ul>
+        <li
+          v-for=" (page, index) in pages"
+          :key="index"
+          :class="{active: activePage==index }"
+        >
+          <!--key is identifier for links to help vue know where it is-->
+          <a
+            :href="page.link.url"
+            :title="`this link goes to ${page.link.text} page`"
+            @click.prevent="activePage = index"
+          >
+            <!--How we bind data from the js-->
+            <!--class will take the value active if the index is = activepage-->
+            {{ page.link.text }}
+          </a>
+        </li>
+      </ul>
+      <form>
+        <button class="btn-toggle" @click.prevent="useDarkNav = !useDarkNav">
+          Background Toggle
+        </button>
+      </form>
+    </nav>
+</template>
+
+<script>
+export default {
+    computed: {
+        navBarClasses() {
+          return {
+            darkNav: this.useDarkNav,
+          };
+        },
+      }
+}
+</script>

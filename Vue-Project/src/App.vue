f2967bb71eadfb1ca0ce4b8560526745c15783f2
Abdullah-khallaf <abdullah.khallaf.work@gmail.com>
2023-05-19 20:49:42 +0300

    new changes

--- Vue-Project/src/App.vue
+++ Vue-Project/src/App.vue
@@ -1,30 +1,38 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import Navbar from './components/Navbar.vue';
+import PageViewer from './components/PageViewer.vue';
+
+export default {
+components: {
+  Navbar,
+  PageViewer,
+},
+data() {
+  return {
+    activePage: 0,
+    useDarkNav: false,
+    pages: [
+      {
+        link: { text: "Home", url: "index.html" },
+        pageTitle: "Home",
+        content: "This Home Page",
+      },
+      {
+        link: { text: "About", url: "about.html" },
+        pageTitle: "About",
+        content: "This About Page",
+      },
+      {
+        link: { text: "Contact", url: "contact.html" },
+        pageTitle: "Contact",
+        content: "This Contact Page",
+      },
+    ],
+  };
+},
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
